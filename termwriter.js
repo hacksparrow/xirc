30d9ea59149a533f29475f00c7a3164d48a37398
mayank_gupta <mayank.gupta@sourcebits.com>
2013-06-27 15:14:23 +0530

    added single print function

--- termwriter.js
+++ termwriter.js
@@ -4,26 +4,27 @@ var NL = '\n';
 var writing = false;
 var lines = 0;
 
-// Write a new line to the console
-exports.line = function(string) {
-  if (writing) return;
-
-  writing = true;
-  stdout.write(string + NL);
-  writing = false;
-};
-
-// Write a text to the console - no new line
-exports.write = function print(string) {
+//Print a string to console
+var print = function(string) {
   if (writing) return;
 
   writing = true;
   stdout.write(string);
   writing = false;
+}
+
+// Write a new line to the console
+exports.line = function(string) {
+  print(string + NL);
+};
+
+// Write a text to the console - no new line
+exports.write = function (string) {
+  print(string);
 };
 
 // Clear everything from the console
 exports.clear = function(buffer) {
-  stdout.write('\u001B[2J\u001B[0;0f');
+  print('\u001B[2J\u001B[0;0f');
 };
 
